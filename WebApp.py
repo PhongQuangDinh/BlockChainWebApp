dc444bd88084b268c3e502ca3ef3bc956ec2a581
PhongQuangDinh <97892951+PhongQuangDinh@users.noreply.github.com>
2023-10-16 23:51:06 +0700

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -30,4 +30,4 @@ def get_chain():
     # result += json.dumps({"blockchain length": len(chain_data), "chain_data": chain_data})
     return result
 
-app.run(debug=True) # port=5000 or something idk but I didnt set port since I need port of Render
+app.run(debug=True, host='0.0.0.0') # port=5000 or something idk but I didnt set port since I need port of Render
